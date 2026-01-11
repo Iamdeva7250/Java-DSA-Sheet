58a278cfafc1b43ac8a687442cf28ef0cfc8fc09
Devanand Singh <127283697+Iamdeva7250@users.noreply.github.com>
2026-01-11 09:09:38 +0530

    11 / 01 / 26

---
+++ ProblemOfTheDay/MinimumWindowSubsequence.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class MinimumWindowSubsequence {
+    
+
+     static String minWindow(String s1, String s2) {
+        int i=0,j=0,k=0;
+        int res=Integer.MAX_VALUE;
+        
+        String str="";
+        
+        while(j<s1.length() && k<s2.length()){
+            if(s1.charAt(j)==s2.charAt(k))k++;
+            if(k==s2.length()){
+                i=j;
+                
+                k=s2.length()-1;
+                while(i>=0 && k>=0){
+                    if(s1.charAt(i)==s2.charAt(k))k--;
+                    i--;
+                }
+                i++;
+                if(res>j-i+1){
+                    res=j-i+1;
+                    str=s1.substring(i,j+1);  
+                }
+                k=0;j=i+1;
+                
+            }
+            j++;
+        }
+        return str;
+    }
+
+
+    public static void main(String[] args) {
+        MinimumWindowSubsequence solution = new MinimumWindowSubsequence();
+        String s1 = "abcdebdde";
+        String s2 = "bde";
+        String result = solution.minWindow(s1, s2);
+        System.out.println("Minimum Window Subsequence: " + result);
+    }
+    
+}
