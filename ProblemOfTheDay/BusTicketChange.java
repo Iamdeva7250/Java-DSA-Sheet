069254d656e336867646509ef79fd1dc88f02a3f
Devanand Singh <127283697+Iamdeva7250@users.noreply.github.com>
2026-01-13 07:56:35 +0530

    13 / 01 / 26

---
+++ ProblemOfTheDay/BusTicketChange.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class BusTicketChange {
+
+    public static boolean canServe(int[] arr) {
+
+        int five = 0, ten = 0;
+
+        for (int note : arr) {
+
+            if (note == 5) {
+                five++;
+            }
+            else if (note == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            }
+            else { // note == 20
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3) {
+                    five -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 5, 5, 10, 20};
+
+        System.out.println(canServe(arr)); 
+    }
+}
